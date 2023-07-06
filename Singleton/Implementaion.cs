53969f8681db3c160a7bfe26b91e90a056b40f89
amr mahmoud <amrmahmoudfcb11@gmail.com>
2023-07-06 10:03:04 +0300

    first commit

---
+++ Singleton/Implementaion.cs
@@ -0,0 +1,21 @@
+﻿namespace Singleton;
+
+public class Logger
+{
+    /// <summary>
+    /// Using Lazy<T> for thread safety
+    /// </summary>
+    private static readonly Lazy<Logger> _instance = new Lazy<Logger>(() => new Logger());
+
+    protected Logger() { }
+
+    public static Logger Instance
+    {
+        get { return _instance.Value; }
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿using Singleton;
+
+Logger logger1 = Logger.Instance;
+Logger logger2 = Logger.Instance;
+
+if (logger1 == logger2)
+{
+    logger2.Log("same");
+}
+
+logger1.Log("hello from logger1");
+logger2.Log("hello from logger2");
